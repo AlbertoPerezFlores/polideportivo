61eb9610b5062f38c72c06d13a8a78889f8802f6
AlbertoPerezFlores <5585180@alu.murciaeduca.es>
2023-04-04 10:03:33 +0200

    modificaciones login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,14 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\HttpFoundation\Request;
 
-class LoginController extends AbstractController
-{
+  class LoginController extends AbstractController
+  {
     /**
      * @Route("/login", name="app_login")
      */
-     public function login(Request $request, AuthenticationUtils $authenticationUtils)
+
+
+     public function login(AuthenticationUtils $authenticationUtils): Response
      {
           if ($this->getUser()) {
               return $this->redirectToRoute('app_home');
@@ -36,4 +37,5 @@ class LoginController extends AbstractController
          
         //  throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
      }
-}
+
+  }
