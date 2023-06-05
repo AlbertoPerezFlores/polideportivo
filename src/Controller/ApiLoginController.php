d7c30aff00c4edcdadd291038f944b168d97ef98
Albertoperezflores <5585180@alu.murciaeduca.es>
2023-06-06 00:53:05 +0200

    apilogincontroller

--- src/Controller/ApiLoginController.php
+++ src/Controller/ApiLoginController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-class ApiController extends AbstractController
+class ApiLoginController extends AbstractController
 {
     /**
      * @Route("/api/login", name="app_api_login")
